42fb5c81f4e60d9c0b151a11fe500eaef383bfa9
Mhmod <mahmoud97787@gmail.com>
2024-08-30 18:35:29 +0300

    add images

---
+++ lib/utils/app_images.dart
@@ -0,0 +1,33 @@
+// ignore_for_file: prefer_single_quotes
+class AppImages {
+  AppImages._();
+  
+  /// Assets for business
+  /// assets/business.avif
+  static const String business = "assets/business.avif";
+
+  /// Assets for entertainment
+  /// assets/entertainment.avif
+  static const String entertainment = "assets/entertainment.avif";
+
+  /// Assets for general
+  /// assets/general.avif
+  static const String general = "assets/general.avif";
+
+  /// Assets for health
+  /// assets/health.avif
+  static const String health = "assets/health.avif";
+
+  /// Assets for science
+  /// assets/science.avif
+  static const String science = "assets/science.avif";
+
+  /// Assets for sports
+  /// assets/sports.avif
+  static const String sports = "assets/sports.avif";
+
+  /// Assets for technology
+  /// assets/technology.jpeg
+  static const String technology = "assets/technology.jpeg";
+}
+
